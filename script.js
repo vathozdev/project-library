ed0d3bebda73d38737955cb315b41a8c202ed1ad
vathozdev <271198640+vathozdev@users.noreply.github.com>
2026-08-09 10:46:23 +0300

    Define Book constructor in script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const myLibrary = [];
+
+function Book(title, author, page, id, status) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.id = id;
+    this.status = status;
+}
