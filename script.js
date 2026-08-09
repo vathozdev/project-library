e4f6f3327492b544bda1bda5906e7c804f719a5c
vathozdev <271198640+vathozdev@users.noreply.github.com>
2026-08-09 11:37:30 +0300

    Add form submission handling to script.js

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ function Book(title, author, page, id, status) {
 Book.prototype.changeStatus = function() {
     this.status = !this.status
 }
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const formValues = Object.fromEntries(formData);
+});
