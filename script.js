81ba6e7733b1628826e870a238a095179856503d
vathozdev <271198640+vathozdev@users.noreply.github.com>
2026-08-10 15:27:49 +0300

    Refactor displayBooks function to improve table row creation and ensure proper appending of elements

--- script.js
+++ script.js
@@ -28,15 +28,50 @@ myLibrary.push(new Book(formValues.title, formValues.author, Number(formValues.p
 }
 function displayBooks() {
     const table = document.createElement("table");
-        container.appendChild(table)
+    container.appendChild(table);
+
     const thead = document.createElement("thead");
-        table.appendChild(thead);
+    table.appendChild(thead);
+
     const tbody = document.createElement("tbody");
-        table.appendChild(tbody);
+    table.appendChild(tbody);
+
     for (const book of myLibrary) {
         const newRow = document.createElement("tr");
+
         const cell = document.createElement("td");
-            newRow.appendChild(cell);
-            tbody.appendChild(newRow);
-      }
-} 
+        newRow.appendChild(cell);
+        cell.textContent = book.title;
+
+        const cell2 = document.createElement("td");
+        newRow.appendChild(cell2);
+        cell2.textContent = book.author;
+
+        const cell3 = document.createElement("td");
+        newRow.appendChild(cell3);
+        cell3.textContent = book.page;
+
+        const cell4 = document.createElement("td");
+        newRow.appendChild(cell4);
+        cell4.textContent = book.id;
+
+        const cell5 = document.createElement("td");
+        newRow.appendChild(cell5);
+
+        if (book.status === false) {
+            cell5.textContent = "not yet read";
+        } else {
+            cell5.textContent = "read";
+        }
+
+        const cell6 = document.createElement("td");
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "REMOVE";
+
+        cell6.appendChild(removeBtn);
+        newRow.appendChild(cell6);
+
+        tbody.appendChild(newRow);
+    }
+}
