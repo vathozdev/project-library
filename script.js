7a5fb20a967d4a08f73b8f6cc74a6b9e6a5a3d6a
vathozdev <271198640+vathozdev@users.noreply.github.com>
2026-08-11 13:15:44 +0300

    Refactor form submission handling to improve organization and clarity

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ cancelBtn.addEventListener("click", () => {
 Book.prototype.changeStatus = function() {
     this.status = !this.status
 }
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => { 
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const formValues = Object.fromEntries(formData);
+    addBookToLibrary(formValues);
+    displayBooks();
+});
 
 function addBookToLibrary(formValues) {
 
@@ -87,13 +97,3 @@ function displayBooks() {
         tbody.appendChild(newRow);
     }
 }
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (event) => { 
-    event.preventDefault();
-
-    const formData = new FormData(form);
-    const formValues = Object.fromEntries(formData);
-    addBookToLibrary(formValues);
-    displayBooks();
-});
